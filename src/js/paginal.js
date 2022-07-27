c3df73cb67adb09f4fcf183830641c3688354e23
oLORDer <justsendmeow@gmail.com>
2022-07-27 14:23:04 +0300

    pag

--- src/js/paginal.js
+++ src/js/paginal.js
@@ -1,29 +1,39 @@
+'use strict'
 import {ticketmasterAPI, renderBaseMarkup} from './render-base-markup';
+{/* <ul class="paginal"></ul> */}
 
 const list = document.querySelector('.paginal');
-let currentPageForCheck;
+let currentPageForCheck = 1;
 
 function paginal(totalObjects, numberImgPerPage, currentPage) {
   const sum = Math.ceil(totalObjects / numberImgPerPage);
   let btnsArr = [];
   currentPageForCheck = currentPage + 1;
-  // console.log(currentPage)
 
   for(let i = 1; i <= sum; i += 1) {
+    if(i === currentPageForCheck) {
+      btnsArr.push(`<li class="act">${i}</li>`);
+      continue;
+    }
     btnsArr.push(`<li>${i}</li>`);
   }
 
   let len = btnsArr.length;
 
-  if(currentPage > 4) {
-    list.innerHTML = btnsArr[0] + '..' + btnsArr[currentPage - 1] + btnsArr.slice(currentPage, currentPage + 3).join('') + '.....' +  btnsArr[len - 1];
-    console.log('5')
+  if(currentPage > 2) {
+    if(currentPage >= len - 3) {
+      list.innerHTML = btnsArr[0] + btnsArr[currentPage - 1] + btnsArr.slice(currentPage, currentPage + 3).join('');
+    } else {
+      list.innerHTML = btnsArr[0] + '...' + btnsArr[currentPage - 1] + btnsArr.slice(currentPage, currentPage + 3).join('') + '...' +  btnsArr[len - 1];
+    }
   } else if(currentPage > 0) {
-    list.innerHTML = btnsArr[currentPage - 1] + btnsArr.slice(currentPage, currentPage + 3).join('') + '.....' +  btnsArr[len - 1];
-    console.log('kyky')
+    list.innerHTML = btnsArr[currentPage - 1] + btnsArr.slice(currentPage, currentPage + 3).join('') + '...' +  btnsArr[len - 1];
   } else {
-    list.innerHTML = btnsArr.slice(currentPage, currentPage + 3).join('') + '.....' +  btnsArr[len - 1];
+    list.innerHTML = btnsArr.slice(currentPage, currentPage + 3).join('') + '...' +  btnsArr[len - 1];
   }
+
+
+
 
   list.addEventListener('click', paginClick);
 }
@@ -32,10 +42,8 @@ function paginal(totalObjects, numberImgPerPage, currentPage) {
 
 function paginClick(e) {
   if (e.target.nodeName !== 'LI' || e.target.textContent == currentPageForCheck) {return}
-  console.log(currentPageForCheck)
   ticketmasterAPI.page = e.target.textContent - 1;
   renderBaseMarkup();
-  // console.log(e.target.textContent)
 }
-// console.log(ticketmasterAPI.page);
+
 export { paginal };
