b684a7d48ef627f4323d907032a9a7b4a5d72c23
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-28 16:05:37 +0300

    checkpoint

--- src/js/render-base-markup.js
+++ src/js/render-base-markup.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { ticketModal } from '../templates/modal-card';
 import { ticketMarkup } from '../templates/gallery';
 import { TicketmasterAPI } from './ticketmaster-api';
 import { paginal } from './paginal';
@@ -9,6 +10,8 @@ const ticketmasterAPI = new TicketmasterAPI();
 const galleryEl = document.querySelector('.gallery');
 const searchQueryEl = document.querySelector('.js-serch-query');
 const searchCountryEl = document.querySelector('.country-input');
+
+const modalEl = document.querySelector('.for-modal-js');
 
 renderBaseMarkup();
 
@@ -36,6 +39,54 @@ async function renderBaseMarkup() {
       .join('');
 
     galleryEl.innerHTML = baseMarkup;
+
+    //? >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    let inputs = galleryEl.getElementsByTagName('LI');
+
+    for (let i = 0; i < inputs.length; i += 1) {
+      inputs[i].addEventListener('click', onTargetElementClick);
+    }
+
+    function onTargetElementClick() {
+      let modalCardMarkup = null;
+      response._embedded.events.forEach(el => {
+        if (this.dataset.id === el.id) {
+          return (modalCardMarkup = ticketModal(el));
+        }
+      });
+      modalEl.innerHTML = modalCardMarkup;
+      document.body.classList.add('no-scroll');
+
+      const closeModalBtn = document.querySelector('.modal__close-btn');
+      const backdropEl = document.querySelector('.modal');
+
+      window.addEventListener('keydown', onEscBtnPush);
+      backdropEl.addEventListener('click', onBackdropElClick);
+      closeModalBtn.addEventListener('click', closeModalWindow);
+
+      function onBackdropElClick(e) {
+        if (e.target !== e.currentTarget) {
+          return;
+        }
+        closeModalWindow();
+      }
+
+      function onEscBtnPush(e) {
+        if (e.code !== 'Escape') {
+          return;
+        }
+        closeModalWindow();
+      }
+
+      function closeModalWindow() {
+        modalEl.innerHTML = '';
+        document.body.classList.remove('no-scroll');
+        window.removeEventListener('keydown', onEscBtnPush);
+      }
+    }
+
+    //? <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     paginal(
       response.page.totalElements,
--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -3,25 +3,18 @@
 export function ticketMarkup(el) {
   return `<li data-id="${el.id}" class= "gallery__item">
 
-  <div class="">
+  <div class="js-target">
     <picture>
-      <source
-        srcset="
-        ${el.images[0].url} 1x,
-        ${el.images[0].url} 2x
-        "
-        type="image/jpg"
-      />
       <img
-      class="gallery__img js-target"
+      class="gallery__img"
         src="${el.images[0].url}"
         alt="фото веб-сайта"
       />
     </picture>
 
     <div class="gallery__wrap">
-      <h2 class="gallery__title js-target"> ${el.name}</h2>
-      <p class="gallery__txt js-target">${el.dates.start.localDate}</p>
+      <h2 class="gallery__title"> ${el.name}</h2>
+      <p class="gallery__txt">${el.dates.start.localDate}</p>
     </div>
   </div>
 
