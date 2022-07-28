737d33c7a4bfcc24729c9462a1a14811ca73ae67
Sergey Gulaga <ry6ens@gmail.com>
2022-07-28 18:47:36 +0300

    add styles + js modal

--- src/js/render-base-markup.js
+++ src/js/render-base-markup.js
@@ -51,6 +51,7 @@ async function renderBaseMarkup() {
       let modalCardMarkup = null;
       response._embedded.events.forEach(el => {
         if (this.dataset.id === el.id) {
+          console.log(el);
           return (modalCardMarkup = ticketModal(el));
         }
       });
