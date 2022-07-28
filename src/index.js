7a1871813847f58a13d1209a7de4223d0dad3f73
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-28 22:18:00 +0300

    add scroll btn

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import paginal from './js/paginal';
 import theme from './js/theme';
 import modalCard from './js/modal-cards';
 import countrySelector from './js/country-selector';
+import scroll from './js/scroll';
