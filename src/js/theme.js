64d6128e698c2e3fe8fb1bd8f6f653b579d36802
oLORDer <justsendmeow@gmail.com>
2022-07-29 14:17:07 +0300

    paginal/theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,13 +1,21 @@
 'use strict'
 const btn = document.querySelector('.switch')
+const paginalBtns = document.querySelector('.paginal')
+
+if(localStorage.getItem('theme') === 'light') {
+  document.body.classList.add('light')
+  btn.checked = true;
+  paginalBtns.classList.add('lightTheme')
+}
 
 btn.addEventListener('click', () => {
     if(localStorage.getItem('theme') === 'light'){
-
         localStorage.removeItem('theme');
+        paginalBtns.classList.remove('lightTheme')
     } else {
         localStorage.setItem ('theme', 'light');
-        btn.checked
+        btn.checked = true;
+        paginalBtns.classList.add('lightTheme')
     }
     addLightClassToBody()
 })
@@ -22,7 +30,7 @@ function addLightClassToBody() {
 
 addLightClassToBody()
 
-document.getElementById('theme-toggle').addEventListener('click', (e) => {
-    const checked = e.target.checked;
-    document.body.setAttribute('theme', checked ? 'light' : 'dark');
-  });
+// document.getElementById('theme-toggle').addEventListener('click', (e) => {
+//     const checked = e.target.checked;
+//     document.body.setAttribute('theme', checked ? 'light' : 'dark');
+//   });
