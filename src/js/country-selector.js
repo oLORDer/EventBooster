4e6458d2307c9cbb93225e21d68930dfce51b315
zerkel1991 <acorus1991@gmail.com>
2022-07-29 10:54:00 +0300

    complete country-select

--- src/js/country-selector.js
+++ src/js/country-selector.js
@@ -1,16 +1,20 @@
 'use strict';
 const selected = document.querySelector('.selected');
 const optionsContainer = document.querySelector('.options-container');
-
 const optionsList = document.querySelectorAll('.option');
-
+let inputId = "";
 selected.addEventListener('click', () => {
   optionsContainer.classList.toggle('active');
 });
 
-optionsList.forEach(o => {
-  o.addEventListener('click', () => {
-    selected.innerHTML = o.querySelector('label').innerHTML;
+optionsList.forEach(element => {
+  element.addEventListener('click', (event) => {
+    let inputId = event.target.id;
+    selected.innerHTML = element.querySelector('label').innerHTML;
+    console.log(inputId)
     optionsContainer.classList.remove('active');
-  });
 });
+});
+
+
+
