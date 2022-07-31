10894f7b1bdf145017631b488c945c56dfdc4f2c
Sergey Gulaga <ry6ens@gmail.com>
2022-07-31 18:32:12 +0300

    add

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -5,9 +5,16 @@ export function ticketMarkup(el) {
   if (el.name.length > 20) {
     nameArtist = el.name.slice(0, 20) + '...';
   }
+  // console.log(el._embedded);
 
   if (!el._embedded.venues[0].name) {
     el._embedded.venues[0].name = haventPlace;
+  }
+
+  if (!el._embedded.venues[0].location) {
+    el._embedded.venues[0].location = [{}];
+    el._embedded.venues[0].location.latitude = '';
+    el._embedded.venues[0].location.longitude = '';
   }
 
   return `<li class= "gallery__item">
